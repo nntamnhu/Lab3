72a9e1f34a97aea7badfd4f47d6c5080bd08d066
LENOVO <nntamnhu01@gmail.com>
2023-05-27 11:13:27 +0700

    Bai thuc hanh buoi 4

--- src/main/java/nguyenngoctamnhu/Lab3/controller/BookController.java
+++ src/main/java/nguyenngoctamnhu/Lab3/controller/BookController.java
@@ -1,17 +1,18 @@
 package nguyenngoctamnhu.Lab3.controller;
 
+import jakarta.validation.Valid;
 import nguyenngoctamnhu.Lab3.entity.Book;
+import nguyenngoctamnhu.Lab3.entity.Category;
 import nguyenngoctamnhu.Lab3.services.BookService;
 import nguyenngoctamnhu.Lab3.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/books")
@@ -37,8 +38,31 @@ public class BookController {
     }
 
     @PostMapping("/add")
-    public String addBook(@ModelAttribute("book") Book book) {
+    public String addBook(@Valid @ModelAttribute("book") Book book, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/add";
+        }
+
         bookService.addBook(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBookForm(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.getBookById(id);
+        List<Category> categories = categoryService.getAllCategories();
+
+        model.addAttribute("book", book);
+        model.addAttribute("categories", categories);
+
+        return "book/edit";
+    }
+
+    @PostMapping("/edit")
+    public String editBook(@ModelAttribute("book") Book updateBook) {
+        bookService.updateBook(updateBook);
+
         return "redirect:/books";
     }
 }
